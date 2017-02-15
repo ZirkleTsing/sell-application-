b25427c51c85076608641033b13bd19180a1bb39
Chang <835702580@qq.com>
2017-02-15 20:26:29 +0800

    complement the header except background-img

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,13 +1,123 @@
 <template>
   <div class="header">
-    我是header
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width="64" height="64" :src="seller.avatar"></img>
+        <!--必须使用:src 因为是异步加载src地址，若src，编译时会解析报错-->
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div v-if="seller.supports" class="supports">
+          <!--v-if的原因是由于app.vue异步加载数据，所以一开始的seller为空（未加载完成）-->
+          <span class="icon" :class="classMap[seller.supports[0].type]">{{seller.supports[0].type}}</span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
+    </div>
+    <div class="bulletin-wrapper"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default{};
+  export default{
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
 
+  .header
+    color: #fff
+    background: #999
+    .content-wrapper
+      position: relative
+      padding: 24px 12px 18px 24px
+      font-size: 0
+      .avatar
+        display: inline-block
+        vertical-align: top
+        img
+          border-radius: 2px
+      .content
+        display: inline-block
+        margin-left: 16px
+        .title
+          margin: 2px 0px 8px 0px
+          .brand
+            /*行内元素声明宽高无效，需要设定为block*/
+            vertical-align: top
+            display: inline-block
+            width: 30px
+            height: 18px
+            bg-image('brand')
+            /*brand后mixin会自动补全@2x @3x*/
+            background-size 30px 18px
+            background-repeat: no-repeat
+          .name
+            margin-left: 6px
+            font-size 16px
+            line-height 18px
+            font-weight bold
+        .description
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: 12px
+        .supports
+          .icon
+            display: inline-block
+            vertical-align: top
+            width: 12px
+            height 12px
+            margin-right 4px
+            background-size 12px 12px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            line-height: 12px
+            font-size: 10px
+
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding: 0 8px
+        height 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0, 0, 0, 0.2)
+        text-align: center
+        .count
+          vertical-align: top
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          margin-left: 2px
+          line-height:24px
+          font-size: 10px
 </style>

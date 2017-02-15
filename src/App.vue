df1bcf53ab02700b8ecbf2af14b62193b6a83f56
Chang <835702580@qq.com>
2017-02-15 14:13:17 +0800

    @import the mixin.styl into app.vue

--- src/App.vue
+++ src/App.vue
@@ -27,11 +27,15 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
   .tab
     display: flex
     windth: 100%
     height: 40px
     line-height: 40px
+    /*border-bottom: 1px solid rgba(7,17,21,0.1)*/
+    border-1px(rgba(7, 7, 27, 0.1))
     .tab-item
       flex: 1
       text-align: center

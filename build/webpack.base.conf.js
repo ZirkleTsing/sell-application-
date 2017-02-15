04881d07d2394b6ef2a94d53ad08a8a202dcffa2
Chang <835702580@qq.com>
2017-02-15 14:53:31 +0800

    modified the common's path

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -17,6 +17,7 @@ module.exports = {
     fallback: [path.join(__dirname, '../node_modules')],
     alias: {
       'src': path.resolve(__dirname, '../src'),
+      'common': path.resolve(__dirname, '../src/common'),
       'components': path.resolve(__dirname, '../src/components')
     }
   },

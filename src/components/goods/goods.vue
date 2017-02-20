3ad69642c453f81ec1845ec485377ed676263d74
Chang <835702580@qq.com>
2017-02-20 21:52:23 +0800

    add scroll and goods detail

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="menu">
-    <div class="menu-bar">
+    <div class="menu-bar" v-el:menu-wrapper>
       <ul>
         <li v-for="item in goods" class="detail">
           <span class="detail-content">
@@ -9,18 +9,18 @@
         </li>
       </ul>
     </div>
-    <div class="menu-detail">
+    <div class="menu-detail" v-el:foods-wrapper>
       <ul v-for="item in goods" class="menu-list">
-        <h1>{{item.name}}</h1>
+        <h1 class="title">{{item.name}}</h1>
         <ul class="food-item">
-          <li v-for="food in item.foods">
+          <li v-for="food in item.foods" class="item">
             <div class="icon">
-              <img :src="food.icon">
+              <img width="54" height="54" :src="food.icon">
             </div>
             <div class="info">
               <h2 class="name">{{food.name}}</h2>
               <div class="desc">
-                <div class="desc-detail">{{food.description}}</div>
+                <div v-if="food.description !==''" class="desc-detail">{{food.description}}</div>
                 <div class="extra">
                   <span class="sellCount">月售{{food.sellCount}}份</span>
                   <span>好评率{{food.rating}}%</span>
@@ -30,20 +30,21 @@
                 <span class="now">
                   ￥{{food.price}}
                 </span>
-                <span v-if="food.oldPrice !=='' " class="old">
+                <span v-if="food.oldPrice !==''" class="old">
                   ￥{{food.oldPrice}}
                 </span>
               </div>
             </div>
           </li>
         </ul>
-
       </ul>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+
   const ERR_OK = 0;
 
   export default {
@@ -64,8 +65,18 @@
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
+          this.$nextTick(() => {
+            this._initScroll();
+          });
         }
       });
+    },
+    methods: {
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$els.menuWrapper, {});
+
+        this.foodsScroll = new BScroll(this.$els.foodsWrapper, {});
+      }
     }
   };
 </script>
@@ -92,7 +103,7 @@
         padding: 0 12px
         .detail-content
           display: table-cell
-          width:56px
+          width: 56px
           vertical-align: middle
           border-1px(rgba(7, 17, 27, 0.1))
           font-size: 12px
@@ -117,4 +128,52 @@
     .menu-detail
       flex: 1 1 auto
       width: 100%
+      .menu-list
+        .title
+          padding-left: 14px
+          height: 26px
+          border-left: 2px solid #d9dde1
+          background: #f3f5f7
+          font-size: 12px
+          color: rgb(147, 153, 159)
+          line-height: 26px
+        .food-item
+          .item
+            display: flex
+            margin: 18px
+            padding-bottom: 18px
+            border-1px(rgba(7, 17, 27, 0.1))
+            &:last-child
+              border-none()
+              margin-bottom: 0
+            .icon
+              flex: 0 0 54px
+              margin-right: 10px
+            .info
+              flex: 1 1 auto
+              .name
+                padding: 2px 0 8px 0
+                height: 14px
+                font-size: 14px
+                color: rgb(7, 17, 27)
+                line-height: 14px
+              .desc
+                font-size: 10px
+                color: rgb(147, 153, 159)
+                line-height: 10px
+                margin-bottom: 4px
+                .desc-detail
+                  margin-bottom: 8px
+                .extra
+                  .sellCount
+                    margin-right: 12px
+              .price
+                font-weight: 700
+                line-height: 24px
+                .now
+                  font-size: 14px
+                  color: rgb(240, 20, 20)
+                .old
+                  font-size: 10px
+                  color: rgb(147, 153, 159)
 </style>
